a97c9b4ab1cb2e0f784752563e31ac1b0f1a9317
brousja20 <brousja20@sps-prosek.cz>
2022-08-25 23:25:28 +0200

    tried to route db data->didnt work

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// fetch mysql data to frontend
+// Route::get('/blog', [BlogController::class, 'rtrnAll']);
+    // didnt work, i think i need some api middleware
