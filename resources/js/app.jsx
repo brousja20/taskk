6891c12b74de35ba04ca5151996cb83c2be6cd8e
brousja20 <brousja20@sps-prosek.cz>
2022-09-29 12:35:37 +0200

    login ui

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,10 +1,11 @@
 import './bootstrap'
 
+import '../sass/app.scss'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/App'
-// import Header from './components/Header'
 
 if (document.getElementById('root')) {
     ReactDOM.render(<App />, document.getElementById('root'))
