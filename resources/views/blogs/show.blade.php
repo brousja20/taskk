b40eba15ca910fbbf81097deb7d3e0cc18138639
brousja20 <brousja20@sps-prosek.cz>
2022-09-29 12:37:10 +0200

    comments

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -18,7 +18,7 @@
 
 {{-- html for single blog --}}
 <h2>
-    {{$blog['name']}}
+    Name: {{$blog['name']}}
 </h2>
 <h4>
     By: {{$blog['author']}}
@@ -30,7 +30,99 @@
     {{$blog['updated_at']}}
 </p>
 <p>
-    {{$blog['text']}}
+    Description: {{$blog['text']}}
 </p>
+
+<br>
+<h3>comments</h3>
+<div>
+    <form action="/blogs/{{ $blog->id }}/comments" method="POST">
+        @csrf
+
+        <label for="text">Add comment</label>
+        <textarea name="text"></textarea>
+
+        <input type="hidden" name="parent_id" value="NULL">
+
+        <button type="submit">comment as <strong>{{ auth()->user()->name }}</strong></button>
+    </form>
+</div>
+<br>
+<br>
+<br>
+@foreach ($comments as $comment)
+    <p style="display: inline"><strong>{{$comment->user['name']}}: </strong>{{ $comment->text }}</p>
+
+    <form action="/blogs/{{ $blog->id }}/comments" method="POST" style="display: inline">
+        @csrf
+
+        <textarea name="text"></textarea>
+
+        <input type="hidden" name="parent_id" value="{{ $comment->id }}">
+
+        <button type="submit">reply</button>
+    </form>
+
+    @if ($comment->user_id == auth()->id())
+        <form action="/comments/{{ $comment->id }}" method="POST" style="display: inline">
+            @csrf
+            @method('DELETE')
+
+            <button style="background-color: red" type="submit">delete</button>
+        </form>
+    @endif
+    
+    @foreach ($comment->replies as $reply)
+        <br>
+        <p style="margin-left: 18px; display: inline">->{{$reply->user['name']}}: {{$reply->text}} </p>
+
+        <form action="/blogs/{{ $blog->id }}/comments" method="POST" style="display: inline">
+            @csrf
+    
+            <textarea name="text"></textarea>
+    
+            <input type="hidden" name="parent_id" value="{{ $reply->id }}">
+    
+            <button type="submit">reply</button>
+        </form>
+
+        @if ($reply->user_id == auth()->id())
+            <form action="/comments/{{ $reply->id }}" method="POST" style="display: inline">
+                @csrf
+                @method('DELETE')
+
+                <button style="background-color: red" type="submit">delete</button>
+            </form>
+        @endif
+
+        @foreach ($reply->replies as $reply)
+            <br>
+            <p style="margin-left: 50px; display: inline"">->{{$reply->user['name']}}: {{$reply->text}} </p>
+
+            <form action="/blogs/{{ $blog->id }}/comments" method="POST" style="display: inline">
+                @csrf
+        
+                <textarea name="text"></textarea>
+        
+                <input type="hidden" name="parent_id" value="{{ $reply->id }}">
+        
+                <button type="submit">reply</button>
+            </form>
+
+            @if ($reply->user_id == auth()->id())
+                <form action="/comments/{{ $reply->id }}" method="POST" style="display: inline">
+                    @csrf
+                    @method('DELETE')
+
+                    <button style="background-color: red" type="submit">delete</button>
+                </form>
+            @endif
+
+        @endforeach
+
+    @endforeach
+    <br>
+    <br>
+@endforeach
 
 @endsection
